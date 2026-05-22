195ed1c63606580ab3f0deadf79479729f132aee
lisantov <lisantov@yandex.ru>
2026-05-22 18:38:48 +0700

    feat(users): added user entity with necessary fields

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [],
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,62 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+enum IGender {
+  MALE = 'male',
+  FEMALE = 'female',
+}
+
+enum IRole {
+  USER = 'user',
+  ADMIN = 'admin',
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  about: string;
+
+  @Column({
+    type: 'datetime',
+  })
+  birthdate: Date;
+
+  @Column()
+  city: string;
+
+  @Column({
+    enum: IGender,
+  })
+  gender: IGender;
+
+  @Column()
+  avatar: string;
+
+  @Column('simple-array')
+  skills: string[];
+
+  @Column('simple-array')
+  wantToLearn: string[];
+
+  @Column('simple-array')
+  favoriteSkills: string[];
+
+  @Column({
+    enum: IRole,
+  })
+  role: IRole;
+
+  @Column()
+  refreshToken: string;
+}
