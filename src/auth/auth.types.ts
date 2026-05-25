e70e0b6d7065bc0e2c99fbd90b49841ab9e472be
mstitovgit <73519508+mstitovgit@users.noreply.github.com>
2026-05-25 15:21:44 +0300

    feat(auth): add jwt payload and auth request types

---
+++ src/auth/auth.types.ts
@@ -0,0 +1,16 @@
+import { Request } from 'express';
+
+enum UserRole {
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+}
+
+export type TJwtPayload = {
+  sub: string;
+  email: string;
+  role: UserRole;
+};
+
+export type AuthRequest = Request & {
+  user: TJwtPayload;
+};
