a6123ef701ff97e0f3e25082c75279ccb1ef96c6
Egor Shesterkin <ewill1989@yandex.ru>
2026-06-02 21:06:29 +0300

    feat(errors): подключен фильтр ошибок

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import appConfig, { TAppConfig } from './config/app.config';
+import { AllExceptionFilter } from './common/filters/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   const config: TAppConfig = app.get(appConfig.KEY);
   await app.listen(config.port);
