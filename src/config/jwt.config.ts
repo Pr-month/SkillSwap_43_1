88c5198afc49556949eb912a3965915c7589839f
Egor Shesterkin <ewill1989@yandex.ru>
2026-06-02 20:53:28 +0300

    fix(auth): inject jwtConfig

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,14 +1,30 @@
 import { ConfigType, registerAs } from '@nestjs/config';
+import type { JwtSignOptions } from '@nestjs/jwt';
 
-export const jwtConfig = registerAs('jwt', () => ({
-  access: {
-    secret: process.env.JWT_ACCESS_SECRET,
-    expiresIn: process.env.JWT_ACCESS_EXPIRES_IN,
-  },
-  refresh: {
-    secret: process.env.JWT_REFRESH_SECRET,
-    expiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
-  },
-}));
+type JwtTokenConfig = {
+  secret: string;
+  expiresIn: JwtSignOptions['expiresIn'];
+};
+
+type JwtConfig = {
+  access: JwtTokenConfig;
+  refresh: JwtTokenConfig;
+};
+
+export const jwtConfig = registerAs(
+  'jwt',
+  (): JwtConfig => ({
+    access: {
+      secret: process.env.JWT_ACCESS_SECRET || 'super-secret-access-key',
+      expiresIn: (process.env.JWT_ACCESS_EXPIRES_IN ||
+        '15m') as JwtSignOptions['expiresIn'],
+    },
+    refresh: {
+      secret: process.env.JWT_REFRESH_SECRET || 'super-secret-refresh-key',
+      expiresIn: (process.env.JWT_REFRESH_EXPIRES_IN ||
+        '7d') as JwtSignOptions['expiresIn'],
+    },
+  }),
+);
 
 export type TJwtConfig = ConfigType<typeof jwtConfig>;
