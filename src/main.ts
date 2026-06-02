8154e7bf3c12bd5e45a6548c2f769819cb0c6fcf
mstitovgit <73519508+mstitovgit@users.noreply.github.com>
2026-06-02 16:21:39 +0200

    fix lint

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import appConfig, { TAppConfig } from './config/app.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(process.cwd(), 'public'));
   app.useGlobalPipes(
     new ValidationPipe({
