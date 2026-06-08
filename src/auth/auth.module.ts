203852e65957485e6046e2f306f8d3fad7c06bfa
Roland <machodin@yandex.ru>
2026-06-08 18:42:53 +0300

    restore refresh guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { AccessTokenGuard } from './guards/accessToken.guard';
+import { RefreshTokenGuard } from './guards/refreshToken.guard';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { AccessTokenStrategy } from './strategies/accessToken.strategy';
+import { RefreshTokenStrategy } from './strategies/refreshToken.strategy';
 import { JwtModule } from '../jwt/jwt.module';
 import { UsersModule } from '../users/users.module';
 import { RolesGuard } from './guards/role.guard';
@@ -15,7 +17,14 @@ import { RolesGuard } from './guards/role.guard';
     UsersModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, AccessTokenGuard, AccessTokenStrategy, RolesGuard],
-  exports: [AccessTokenGuard, RolesGuard],
+  providers: [
+    AuthService,
+    AccessTokenGuard,
+    RefreshTokenGuard,
+    AccessTokenStrategy,
+    RefreshTokenStrategy,
+    RolesGuard,
+  ],
+  exports: [AccessTokenGuard, RefreshTokenGuard, RolesGuard],
 })
 export class AuthModule {}
