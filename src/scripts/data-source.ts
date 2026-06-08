d13baa7c18584b5ae8f513e6f3b407fbcaeb1619
zeawale <miss.krugletsova@gmail.com>
2026-06-08 17:28:41 +0300

    refactor(seeding): переиспользовать database.config в data-source

--- src/scripts/data-source.ts
+++ src/scripts/data-source.ts
@@ -1,15 +1,5 @@
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
-import { User } from '../users/entities/user.entity';
-import { Skill } from '../skills/entities/skill.entity';
+import dbConfig from '../config/database.config';
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: parseInt(process.env.DATABASE_PORT as string, 10) || 5432,
-  database: process.env.DATABASE_NAME || 'db',
-  username: process.env.DATABASE_USERNAME || 'db',
-  password: process.env.DATABASE_PASSWORD,
-  entities: [User, Skill],
-  synchronize: false,
-});
+export const AppDataSource = new DataSource(dbConfig());
